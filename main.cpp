7f2179649f5865147ea90a469e6c4be8bf953d22
Mathswiz101 <48220990+Mathswiz101@users.noreply.github.com>
2020-11-25 20:20:47 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "game.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <time.h>
+#include<stdlib.h>
+#include<Windows.h>
+#include<MMSystem.h>
+using namespace std;
+
+string name;
+int main()
+{
+    game player1;
+    game player2;
+    int PlayNum;
+    player1.Play();
+    
+    return 0;
+}
